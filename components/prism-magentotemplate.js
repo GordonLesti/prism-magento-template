31cdeb3d2425704c49ce845bc86fcdde71148a47
Gordon Lesti <info@gordonlesti.com>
2016-11-10 17:59:45 +0100

    prism-magentotemplate grammer

--- components/prism-magentotemplate.js
+++ components/prism-magentotemplate.js
@@ -1,4 +1,4 @@
-Prism.languages.magentotemplate = {
+var grammer = {
     'tag': {
         pattern: /\{\{[\s\S]*?\}\}/,
         inside: {
@@ -21,7 +21,7 @@ Prism.languages.magentotemplate = {
 					'punctuation': /^['"]|['"]$/
 				}
 			},
-            'keyword': /\b(?:raw)\b/,
+            'keyword': /\b(?:raw|escape|nl2br)\b/,
             'operator': /=/,
             'variable': /\$\w+\b/i,
             'property': /\b[a-zA-Z_][a-zA-Z0-9_]*\b/,
@@ -29,3 +29,53 @@ Prism.languages.magentotemplate = {
         }
     }
 };
+
+Prism.languages.magentotemplate = grammer;
+
+if (Prism.languages.markup) {
+    Prism.hooks.add('before-highlight', function(env) {
+        if (env.language !== 'magentotemplate') {
+            return;
+        } console.log('before-highlight');
+
+        env.tokenStack = [];
+        env.backupCode = env.code;
+        env.code = env.code.replace(/(?:\{\{)[\w\W]*?(?:\}\})/ig, function(match) {
+            env.tokenStack.push(match);
+            return '{{{MAGENTO' + env.tokenStack.length + '}}}';
+        });
+    });
+
+    Prism.hooks.add('before-insert', function(env) {
+        if (env.language === 'magentotemplate') { console.log('before-insert');
+            env.code = env.backupCode;
+            delete env.backupCode;
+        }
+    });
+
+    Prism.hooks.add('after-highlight', function(env) {
+        if (env.language !== 'magentotemplate') {
+            return;
+        } console.log('after-highlight');
+
+        for (var i = 0, t; t = env.tokenStack[i]; i++) {
+            env.highlightedCode = env.highlightedCode.replace('{{{MAGENTO' + (i + 1) + '}}}', Prism.highlight(t, grammer, 'magentotemplate'));
+        }
+
+        env.element.innerHTML = env.highlightedCode;
+    });
+
+    Prism.hooks.add('wrap', function(env) {
+        if (env.language === 'magentotemplate' && env.type === 'markup') { console.log('wrap'); console.log(env);
+            env.content = env.content.replace(/(\{\{\{MAGENTO[0-9]+\}\}\})/g, "<span class=\"token magentotemplate\">$1</span>");
+        }
+    });
+
+    Prism.languages.insertBefore('magentotemplate', 'tag', {
+        'markup': {
+            pattern: /<[^?]\/?(.*?)>/,
+            inside: Prism.languages.markup
+        },
+        'magentotemplate': /\{\{\{MAGENTO[0-9]+\}\}\}/
+    });
+}
